e7b78531985ed9a7c3b905dbd8212d50a3f21417
Boris Novikov <boris.novikov95@gmail.com>
2022-04-13 19:57:31 +0300

    init

---
+++ App.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Thumbs } from "swiper";
+import "swiper/css";
+export const App = () => {
+  // store thumbs swiper instance
+  const [thumbsSwiper, setThumbsSwiper] = useState(null);
+
+  return (
+    <main>
+      {/* Main Swiper -> pass thumbs swiper instance */}
+      <Swiper modules={[Thumbs]} thumbs={{ swiper: thumbsSwiper }}>
+        {/* ... */}
+      </Swiper>
+
+      {/* Thumbs Swiper -> store swiper instance */}
+      {/* It is also required to set watchSlidesProgress prop */}
+      <Swiper modules={[Thumbs]} watchSlidesProgress onSwiper={setThumbsSwiper}>
+        {/* ... */}
+      </Swiper>
+    </main>
+  );
+};
--- pages/about.js
+++ pages/about.js
@@ -1,26 +1,5 @@
-import React, { useState } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Thumbs } from "swiper";
-import "swiper/css";
-const App = () => {
-  // store thumbs swiper instance
-  const [thumbsSwiper, setThumbsSwiper] = useState(null);
+import { App } from "../App";
 
-  return (
-    <main>
-      {/* Main Swiper -> pass thumbs swiper instance */}
-      <Swiper modules={[Thumbs]} thumbs={{ [swiper]: thumbsSwiper }}>
-        {/* ... */}
-      </Swiper>
-
-      {/* Thumbs Swiper -> store swiper instance */}
-      {/* It is also required to set watchSlidesProgress prop */}
-      <Swiper modules={[Thumbs]} watchSlidesProgress onSwiper={setThumbsSwiper}>
-        {/* ... */}
-      </Swiper>
-    </main>
-  );
-};
 
 export default function A() {
   return <App />;
